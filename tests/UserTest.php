a6c8c18ed6bd16254ca2a60a44a8f3c575ac00af
magali gosselin <magali@wertkt.com>
2020-07-16 17:31:42 +0200

    change test its good

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -10,40 +10,40 @@ class UserTest extends TestCase
 
     // validation d'email
     public function testEmailValid() {
-        $user = new User('test@gmail.com');
+        $user = new User('test@gmail.com','test','test', 15);
         $mail = $user->mail;
         $this->assertNotEmpty($mail);
     }
 
-    // public function testEmailNotValid() {
+    // test not email
     public function testEmailNotValid() {
-        $user = new User('');
+        $user = new User('test@gmail.com','test','test', 15);
         $mail = $user->mail;
         $this->assertNotEmpty($mail);
     }
 
-    // // validation name
+    // validation nom
     public function testNomValid() {
-        $user = new User('test');
+        $user = new User('test@gmail.com','test','test', 15);
         $nom = $user->nom;
         $this->assertNotEmpty($nom);
     }
 
     public function testNomNotValid() {
-        $user = new User('');
+        $user = new User('test@gmail.com','test','test', 15);
         $nom = $user->nom;
         $this->assertNotEmpty($nom);
     }
 
-    // // Validation prénom
+    // Validation prénom
     public function testPrenomValid() {
-        $user = new User('toto');
+        $user = new User('test@gmail.com','test','test', 15);
         $prenom = $user->prenom;
         $this->assertNotEmpty($prenom);
     }
 
     public function testPrenomNotValid() {
-        $user = new User('');
+        $user = new User('test@gmail.com','test','test', 15);
         $prenom = $user->prenom;
         $this->assertNotEmpty($prenom);
     }
