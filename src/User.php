5d04c7743966c99d2f46da0ee929e403330bdd1e
magali gosselin <magali@wertkt.com>
2020-07-16 17:21:24 +0200

    change test and constructeur

--- src/User.php
+++ src/User.php
@@ -3,12 +3,12 @@ namespace App;
 
 class User
 {
-    private $mail;
-    private $prenom;
-    private $nom;
-    private $age;
-
-    public function __construct(string $mail, string $prenom, string $nom, int $age)
+  public $mail = '';
+  public $prenom = '';
+  public $nom = '';
+  public $age = 0;
+  
+  public function __construct(string $mail, string $prenom, string $nom, int $age)
     {
         $this->mail = $mail;
         $this->prenom = $prenom;
@@ -16,12 +16,13 @@ class User
         $this->age = $age;
     }
 
+
     public function isValid(){
-        if(!empty($this->mail) && !empty($this->prenom) && !empty($this->nom) && !empty($this->age)){
-            if(filter_var($this->mail, FILTER_VALIDATE_mail) === false){
+        if(!empty($this->$mail) && !empty($this->$prenom) && !empty($this->$nom) && !empty($this->$age)){
+            if(filter_var($this->$mail, FILTER_VALIDATE_mail) === false){
                 throw new InvalidArgumentException('mail invalid');
             }
-            if($this->age < 13){
+            if($this->$age < 13){
                 throw new InvalidArgumentException('Too young');
             }
         }else{
@@ -30,23 +31,4 @@ class User
         return true;
     }
 
-    public function getmail(): string
-    {
-        return $this->mail;
-    }
-
-    public function getprenom(): string
-    {
-        return $this->prenom;
-    }
-
-    public function getnom(): string
-    {
-        return $this->nom;
-    }
-
-    public function getAge(): int
-    {
-        return $this->age;
-    }
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,43 +7,45 @@ use App\User;
 
 class UserTest extends TestCase
 {
+
     // validation d'email
     public function testEmailValid() {
-        $user = new User();
-        $email = $user->email->add('test@gmail.com');
-        $this->assertNotEmpty('test@gmail.com',$email);
+        $user = new User('test@gmail.com');
+        $mail = $user->mail;
+        $this->assertNotEmpty($mail);
     }
 
+    // public function testEmailNotValid() {
     public function testEmailNotValid() {
-        $user = new User();
-        $email = $user->email->add('test');
-        $this->assertNotEmpty(200,$email);
+        $user = new User('');
+        $mail = $user->mail;
+        $this->assertNotEmpty($mail);
     }
 
-    // validation name
-    public function testnomValid() {
-        $user = new User();
-        $nom = $user->$nom->add('titi');
-        $this->assertNotEmpty('titi',$nom);
+    // // validation name
+    public function testNomValid() {
+        $user = new User('test');
+        $nom = $user->nom;
+        $this->assertNotEmpty($nom);
     }
 
-    public function testnomNotValid() {
-        $user = new User();
-        $nom = $user->$nom->add('titi');
-        $this->assertNotEmpty('toto',$nom);
+    public function testNomNotValid() {
+        $user = new User('');
+        $nom = $user->nom;
+        $this->assertNotEmpty($nom);
     }
 
-    // Validation prénom
+    // // Validation prénom
     public function testPrenomValid() {
-        $user = new User();
-        $prenom = $user->$prenom->prenom('test');
-        $this->assertNotEmpty('test',$prenom);
+        $user = new User('toto');
+        $prenom = $user->prenom;
+        $this->assertNotEmpty($prenom);
     }
 
     public function testPrenomNotValid() {
-        $user = new User();
-        $prenom = $user->$prenom->prenom('test');
-        $this->assertNotEmpty('toto',$prenom);
+        $user = new User('');
+        $prenom = $user->prenom;
+        $this->assertNotEmpty($prenom);
     }
 
 }
